d5fb51b577fd1b59d3dd3c42ecf6d6380bcd558c
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2022-09-23 19:55:05 +0100

    Add cmd arguments

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,46 @@ mod grpc;
 mod rest;
 
 use crate::core::Database;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "dobby", about = "A database as poor as a house elf.")]
+struct Options {
+    #[structopt(parse(from_os_str), help = "path to the database directory")]
+    path: PathBuf,
+
+    #[structopt(long, name = "name", help = "create a new database called <name>")]
+    new: Option<String>,
+
+    #[structopt(long, default_value = "50051", help = "gRPC server port")]
+    grpc_port: u16,
+
+    #[structopt(long, default_value = "8080", help = "REST server port")]
+    rest_port: u16,
+}
 
 #[tokio::main]
 async fn main() {
-    let db = Arc::new(Mutex::new(Database::open("test".into())));
+    let opt = Options::from_args();
 
-    tokio::select! {
-        _ = grpc::serve(Arc::clone(&db), ([0, 0, 0, 0], 50051)) => {},
-        _ = rest::serve(Arc::clone(&db), ([0, 0, 0, 0],  3030)) => {}
+    let db = {
+        let db = if let Some(name) = opt.new {
+            Database::create(opt.path, name)
+        } else {
+            Database::open(opt.path)
+        };
+        Arc::new(Mutex::new(db))
     };
 
-    // TODO: add config / cmd arg for the server port and db name
+    tokio::select! {
+        _ = grpc::serve(Arc::clone(&db), ([0, 0, 0, 0], opt.grpc_port)) => {},
+        _ = rest::serve(Arc::clone(&db), ([0, 0, 0, 0], opt.rest_port)) => {}
+    };
+
+    // TODO: add my custom types
+    // TODO: add unit tests
     // TODO: add logging
     // TODO: return schemas as response to operations over tables
 }
